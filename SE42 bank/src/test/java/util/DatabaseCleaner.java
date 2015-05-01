7f3a4598690d2b1f42ff8bc2bf9d565b1e29d02f
Alexandervc <a.vancuijk@student.fontys.nl>
2015-05-01 09:50:36 +0200

    poging tot

--- SE42 bank/src/test/java/util/DatabaseCleaner.java
+++ SE42 bank/src/test/java/util/DatabaseCleaner.java
@@ -2,8 +2,12 @@ package util;
 
 import bank.domain.Account;
 import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.persistence.EntityManager;
 import javax.persistence.metamodel.EntityType;
+import static org.junit.Assert.*;
+import org.junit.*;
 
 public class DatabaseCleaner {
 
@@ -14,6 +18,15 @@ public class DatabaseCleaner {
 
     public DatabaseCleaner(EntityManager entityManager) {
         em = entityManager;
+    }
+    
+    @Before
+    public void before() {
+        try {
+            clean();
+        } catch (SQLException ex) {
+            Logger.getLogger(DatabaseCleaner.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     public void clean() throws SQLException {
@@ -33,5 +46,18 @@ public class DatabaseCleaner {
     protected String getEntityName(Class<?> clazz) {
         EntityType et = em.getMetamodel().entity(clazz);
         return et.getName();
+    }
+    
+    @Test
+    public void vraag1() {
+        Account account = new Account(111L);
+        em.getTransaction().begin();
+        em.persist(account);
+        //TODO: verklaar en pas eventueel aan
+        assertNull(account.getId());
+        em.getTransaction().commit();
+        System.out.println("AccountId: " + account.getId());
+        //TODO: verklaar en pas eventueel aan
+        assertTrue(account.getId() > 0L);
     }
 }
