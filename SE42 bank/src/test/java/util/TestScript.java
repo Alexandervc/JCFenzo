aa7c470cd3cbb0baa24aa1fc8c89fba375e009b5
xChocoMel <melaniekwetters@hotmail.com>
2015-05-02 15:44:54 +0200

    dingetjes

--- SE42 bank/src/test/java/util/TestScript.java
+++ SE42 bank/src/test/java/util/TestScript.java
@@ -135,22 +135,22 @@ public class TestScript {
     }
     
     /**
-     * 1. Asserties:
-     *    Printline:
-     * 2. -
-     * 3. -
-     * 4. -
+     * 1. Asserties: Equals (balance = 400) & Equals (balance = 400)
+     * 2. Geen
+     * 3. Het account met de ingegeven balance.
+     * 4. Applicatie kan Peristence Context aanpassen.
      */
     @Test
     public void vraag4() {
         Long expectedBalance = 400L;
         Account account = new Account(114L);
         em.getTransaction().begin();
-        account.setBalance(expectedBalance);
         em.persist(account);
-        
+        account.setBalance(expectedBalance);
         em.getTransaction().commit();
-        //TODO: verklaar de waarde van account.getBalance
+        //Door de persist() wordt de informatie in de Persistence Context gezet. 
+        //Je kunt de Persistence Context benaderen vanuit je applicatie.
+        //De balance van het account wordt dan ook aangepast en in de database weggeschreven.
         assertEquals(expectedBalance, account.getBalance());
       
         Long acId = account.getId();
@@ -158,7 +158,7 @@ public class TestScript {
         EntityManager em2 = emf.createEntityManager();
         em2.getTransaction().begin();
         Account found = em2.find(Account.class, acId);
-        //TODO: verklaar de waarde van found.getBalance
+        //Account in de database klopt met ingegeven balance
         assertEquals(expectedBalance, found.getBalance());
     }
     
@@ -171,7 +171,11 @@ public class TestScript {
      */
     @Test
     public void vraag5() {
-        //TODO
+        //In de vorige opdracht verwijzen de objecten account en found naar dezelfde rij in de database. 
+        //Pas een van de objecten aan, persisteer naar de database.
+        //Refresh vervolgens het andere object om de veranderde state uit de database te halen.
+        //Test met asserties dat dit gelukt is.
+        
     }
     
     /**
@@ -181,96 +185,96 @@ public class TestScript {
      * 3. -
      * 4. -
      */
-    @Test
-    public void vraag6() {
-        Account acc = new Account(1L);
-        Account acc2 = new Account(2L);
-        Account acc9 = new Account(9L);
-
-        // scenario 1
-        Long balance1 = 100L;
-        em.getTransaction().begin();
-        em.persist(acc);
-        acc.setBalance(balance1);
-        em.getTransaction().commit();
-        //TODO: voeg asserties toe om je verwachte waarde van de attributen te verifieren.
-        //TODO: doe dit zowel voor de bovenstaande java objecten als voor opnieuw bij de entitymanager opgevraagde objecten met overeenkomstig Id.
-
-
-        // scenario 2
-        Long balance2a = 211L;
-        acc = new Account(2L);
-        em.getTransaction().begin();
-        acc9 = em.merge(acc);
-        acc.setBalance(balance2a);
-        acc9.setBalance(balance2a+balance2a);
-        em.getTransaction().commit();
-        //TODO: voeg asserties toe om je verwachte waarde van de attributen te verifiëren.
-        //TODO: doe dit zowel voor de bovenstaande java objecten als voor opnieuw bij de entitymanager opgevraagde objecten met overeenkomstig Id. 
-        // HINT: gebruik acccountDAO.findByAccountNr
-
-
-        // scenario 3
-        Long balance3b = 322L;
-        Long balance3c = 333L;
-        acc = new Account(3L);
-        em.getTransaction().begin();
-        acc2 = em.merge(acc);
-        assertTrue(em.contains(acc)); // verklaar
-        assertTrue(em.contains(acc2)); // verklaar
-        assertEquals(acc,acc2);  //verklaar
-        acc2.setBalance(balance3b);
-        acc.setBalance(balance3c);
-        em.getTransaction().commit();
-        //TODO: voeg asserties toe om je verwachte waarde van de attributen te verifiëren.
-        //TODO: doe dit zowel voor de bovenstaande java objecten als voor opnieuw bij de entitymanager opgevraagde objecten met overeenkomstig Id.
-
-
-        // scenario 4
-        Account account = new Account(114L);
-        account.setBalance(450L);
-        EntityManager em = emf.createEntityManager();
-        em.getTransaction().begin();
-        em.persist(account);
-        em.getTransaction().commit();
-
-        Account account2 = new Account(114L);
-        Account tweedeAccountObject = account2;
-        tweedeAccountObject.setBalance(650l);
-        assertEquals((Long)650L,account2.getBalance());  //verklaar
-        account2.setId(account.getId());
-        em.getTransaction().begin();
-        account2 = em.merge(account2);
-        assertSame(account,account2);  //verklaar
-        assertTrue(em.contains(account2));  //verklaar
-        assertFalse(em.contains(tweedeAccountObject));  //verklaar
-        tweedeAccountObject.setBalance(850l);
-        assertEquals((Long)650L,account.getBalance());  //verklaar
-        assertEquals((Long)650L,account2.getBalance());  //verklaar
-        em.getTransaction().commit();
-        em.close();
-
-        // Find en clear
-        Account acc1 = new Account(77L);
-        em.getTransaction().begin();
-        em.persist(acc1);
-        em.getTransaction().commit();
-        //Database bevat nu een account.
-
-        // scenario 1        
-        Account accF1;
-        Account accF2;
-        accF1 = em.find(Account.class, acc1.getId());
-        accF2 = em.find(Account.class, acc1.getId());
-        assertSame(accF1, accF2);
-
-        // scenario 2        
-        accF1 = em.find(Account.class, acc1.getId());
-        em.clear();
-        accF2 = em.find(Account.class, acc1.getId());
-        assertSame(accF1, accF2);
-        //TODO verklaar verschil tussen beide scenario's
-    }
+//    @Test
+//    public void vraag6() {
+//        Account acc = new Account(1L);
+//        Account acc2 = new Account(2L);
+//        Account acc9 = new Account(9L);
+//
+//        // scenario 1
+//        Long balance1 = 100L;
+//        em.getTransaction().begin();
+//        em.persist(acc);
+//        acc.setBalance(balance1);
+//        em.getTransaction().commit();
+//        //TODO: voeg asserties toe om je verwachte waarde van de attributen te verifieren.
+//        //TODO: doe dit zowel voor de bovenstaande java objecten als voor opnieuw bij de entitymanager opgevraagde objecten met overeenkomstig Id.
+//
+//
+//        // scenario 2
+//        Long balance2a = 211L;
+//        acc = new Account(2L);
+//        em.getTransaction().begin();
+//        acc9 = em.merge(acc);
+//        acc.setBalance(balance2a);
+//        acc9.setBalance(balance2a+balance2a);
+//        em.getTransaction().commit();
+//        //TODO: voeg asserties toe om je verwachte waarde van de attributen te verifiëren.
+//        //TODO: doe dit zowel voor de bovenstaande java objecten als voor opnieuw bij de entitymanager opgevraagde objecten met overeenkomstig Id. 
+//        // HINT: gebruik acccountDAO.findByAccountNr
+//
+//
+//        // scenario 3
+//        Long balance3b = 322L;
+//        Long balance3c = 333L;
+//        acc = new Account(3L);
+//        em.getTransaction().begin();
+//        acc2 = em.merge(acc);
+//        assertTrue(em.contains(acc)); // verklaar
+//        assertTrue(em.contains(acc2)); // verklaar
+//        assertEquals(acc,acc2);  //verklaar
+//        acc2.setBalance(balance3b);
+//        acc.setBalance(balance3c);
+//        em.getTransaction().commit();
+//        //TODO: voeg asserties toe om je verwachte waarde van de attributen te verifiëren.
+//        //TODO: doe dit zowel voor de bovenstaande java objecten als voor opnieuw bij de entitymanager opgevraagde objecten met overeenkomstig Id.
+//
+//
+//        // scenario 4
+//        Account account = new Account(114L);
+//        account.setBalance(450L);
+//        EntityManager em = emf.createEntityManager();
+//        em.getTransaction().begin();
+//        em.persist(account);
+//        em.getTransaction().commit();
+//
+//        Account account2 = new Account(114L);
+//        Account tweedeAccountObject = account2;
+//        tweedeAccountObject.setBalance(650l);
+//        assertEquals((Long)650L,account2.getBalance());  //verklaar
+//        account2.setId(account.getId());
+//        em.getTransaction().begin();
+//        account2 = em.merge(account2);
+//        assertSame(account,account2);  //verklaar
+//        assertTrue(em.contains(account2));  //verklaar
+//        assertFalse(em.contains(tweedeAccountObject));  //verklaar
+//        tweedeAccountObject.setBalance(850l);
+//        assertEquals((Long)650L,account.getBalance());  //verklaar
+//        assertEquals((Long)650L,account2.getBalance());  //verklaar
+//        em.getTransaction().commit();
+//        em.close();
+//
+//        // Find en clear
+//        Account acc1 = new Account(77L);
+//        em.getTransaction().begin();
+//        em.persist(acc1);
+//        em.getTransaction().commit();
+//        //Database bevat nu een account.
+//
+//        // scenario 1        
+//        Account accF1;
+//        Account accF2;
+//        accF1 = em.find(Account.class, acc1.getId());
+//        accF2 = em.find(Account.class, acc1.getId());
+//        assertSame(accF1, accF2);
+//
+//        // scenario 2        
+//        accF1 = em.find(Account.class, acc1.getId());
+//        em.clear();
+//        accF2 = em.find(Account.class, acc1.getId());
+//        assertSame(accF1, accF2);
+//        //TODO verklaar verschil tussen beide scenario's
+//    }
     
     /**
      * 1. Asserties: 
@@ -279,28 +283,28 @@ public class TestScript {
      * 3. -
      * 4. -
      */
-    @Test
-    public void vraag7() {
-        Account acc1 = new Account(77L);
-        em.getTransaction().begin();
-        em.persist(acc1);
-        em.getTransaction().commit();
-        //Database bevat nu een account.
-
-        // scenario 1        
-        Account accF1;
-        Account accF2;
-        accF1 = em.find(Account.class, acc1.getId());
-        accF2 = em.find(Account.class, acc1.getId());
-        assertSame(accF1, accF2);
-
-        // scenario 2        
-        accF1 = em.find(Account.class, acc1.getId());
-        em.clear();
-        accF2 = em.find(Account.class, acc1.getId());
-        assertSame(accF1, accF2);
-        //TODO verklaar verschil tussen beide scenario's
-    }
+//    @Test
+//    public void vraag7() {
+//        Account acc1 = new Account(77L);
+//        em.getTransaction().begin();
+//        em.persist(acc1);
+//        em.getTransaction().commit();
+//        //Database bevat nu een account.
+//
+//        // scenario 1        
+//        Account accF1;
+//        Account accF2;
+//        accF1 = em.find(Account.class, acc1.getId());
+//        accF2 = em.find(Account.class, acc1.getId());
+//        assertSame(accF1, accF2);
+//
+//        // scenario 2        
+//        accF1 = em.find(Account.class, acc1.getId());
+//        em.clear();
+//        accF2 = em.find(Account.class, acc1.getId());
+//        assertSame(accF1, accF2);
+//        //TODO verklaar verschil tussen beide scenario's
+//    }
     
     /**
      * 1. Asserties: 
@@ -309,21 +313,21 @@ public class TestScript {
      * 3. -
      * 4. -
      */
-    @Test
-    public void vraag8() {
-        Account acc1 = new Account(88L);
-        em.getTransaction().begin();
-        em.persist(acc1);
-        em.getTransaction().commit();
-        Long id = acc1.getId();
-        //Database bevat nu een account.
-
-        em.remove(acc1);
-        assertEquals(id, acc1.getId());        
-        Account accFound = em.find(Account.class, id);
-        assertNull(accFound);
-        //TODO: verklaar bovenstaande asserts
-    }
+//    @Test
+//    public void vraag8() {
+//        Account acc1 = new Account(88L);
+//        em.getTransaction().begin();
+//        em.persist(acc1);
+//        em.getTransaction().commit();
+//        Long id = acc1.getId();
+//        //Database bevat nu een account.
+//
+//        em.remove(acc1);
+//        assertEquals(id, acc1.getId());        
+//        Account accFound = em.find(Account.class, id);
+//        assertNull(accFound);
+//        //TODO: verklaar bovenstaande asserts
+//    }
     
     /**
      * 1. Asserties: 
@@ -334,6 +338,9 @@ public class TestScript {
      */
     @Test
     public void vraag9() {
-        //TODO
+        //Opgave 1 heb je uitgevoerd met @GeneratedValue(strategy = GenerationType.IDENTITY)
+        //Voer dezelfde opdracht nu uit met GenerationType SEQUENCE en TABLE.
+        //Verklaar zowel de verschillen in testresultaat als verschillen van de database structuur.
+        
     }
 }
