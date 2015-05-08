f47a08a7a3a8536acc90aa7f14af98c45f8fe403
xChocoMel <melaniekwetters@hotmail.com>
2015-05-08 15:55:35 +0200

    XML toegevoegd + database "auctionse" gemaakt

--- SE42 auction/auction/src/main/java/auction/domain/User.java
+++ SE42 auction/auction/src/main/java/auction/domain/User.java
@@ -1,12 +1,19 @@
 package auction.domain;
 
-public class User {
+import java.io.Serializable;
+import javax.persistence.*;
 
+@Entity
+public class User implements Serializable {
+
+    @Id
     private String email;
+
+    public User() {
+    }
 
     public User(String email) {
         this.email = email;
-
     }
 
     public String getEmail() {
