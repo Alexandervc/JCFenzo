120c02ee7dc820938f7bba96881159443c2de12a
xChocoMel <melaniekwetters@hotmail.com>
2015-06-16 09:56:59 +0200

    indeling

---
+++ SE42/SE42 bank/target/generated-sources/annotations/bank/domain/Account_.java
@@ -0,0 +1,16 @@
+package bank.domain;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.6.0.v20150309-rNA", date="2015-05-03T16:17:19")
+@StaticMetamodel(Account.class)
+public class Account_ { 
+
+    public static volatile SingularAttribute<Account, Long> balance;
+    public static volatile SingularAttribute<Account, Long> accountNr;
+    public static volatile SingularAttribute<Account, Long> threshold;
+    public static volatile SingularAttribute<Account, Long> Id;
+
+}
